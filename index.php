c8bf1965b285916d3b4e1474383c950dff76d72d
FlashBlaze <neerajlagwankar@gmail.com>
2017-10-28 21:57:10 +0530

    Added favicons and hopefully be the last commit by me

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="Stylesheets/styleIndex.css">
+        <link rel="shortcut icon" type="image/png" href="Stylesheets/Images/favicon1.png">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.7.0/jsmediatags.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         
